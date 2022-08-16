e8d88ad076ab2709cbd0f4fba0e9ce76a490cfc2
JuliusBal <julius.baltusis@gmail.com>
2022-08-16 14:54:54 +0300

    Removed unnecessary files

--- resources/js/global.js
+++ resources/js/global.js
@@ -1,29 +1,3 @@
-var myNodelist = document.getElementsByClassName(".guestList");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
-}
-
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-        var div = this.parentElement;
-        div.remove();
-    }
-}
-
-var list = document.querySelector('ul');
-list.addEventListener('click', function (ev) {
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
-    }
-}, false);
-
 function newElement() {
     if (document.getElementById("guestInput").value === '') {
         alert("Please enter your guest details");
@@ -47,5 +21,14 @@ function newElement() {
             var div = this.parentElement;
             div.remove();
         }
+    }
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+        var div = this.parentElement;
+        div.remove();
     }
 }
